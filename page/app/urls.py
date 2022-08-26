572db43cad237546fdea20f3f3f994e9e863b939
Mohammad Hosein <nadrii638@gmail.com>
2022-08-27 01:16:05 +0430

    some better coding

--- page/app/urls.py
+++ page/app/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('' , views.mainpage, name='mainpage'),
     path('go/<name>', views.go, name='go'),
     path('hello/', views.hello, name='hello'), 
     path('inc/', views.inc, name='inc'), 
     path('salam/<name>', views.salam, name='salam'),
     path('test/', views.test, name='test'),
 ]
-
--- page/page/urls.py
+++ page/page/urls.py
@@ -19,6 +19,5 @@ from django.urls import include , path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-    path('' , views.mainpage, name='mainpage'),
 ]
 
